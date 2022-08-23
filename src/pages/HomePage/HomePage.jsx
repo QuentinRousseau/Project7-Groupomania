1c996036b1aadee8d3fc484f91668c948d05d2ce
Stylys <rousseau.quentin91580@gmail.com>
2022-08-23 16:53:28 +0200

    import files and front begin
    add server.js
    modify var of header.scss
    init loader for charging
    init homepage.jsx & .scss

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Loader } from "../../components/Loader/Loader";
 
 function HomePage() {
+  const [postsList, setpostsList] = useState({});
+  const [isDataLoading, setDataLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    fetch(``);
-  });
-  return <div className="home"></div>;
+    setDataLoading(true);
+    try {
+      //const response = await fetch(`/api/posts`)
+      // const postsList = await response.json()
+      setpostsList(postsList);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    } finally {
+      setDataLoading(false);
+    }
+  }, []);
+
+  if (error) return <span>Oulà , on a un problème !</span>;
+  return (
+    <div className="homePage">
+      <div className="titlebloc">
+        <h1 className="titlebloc__title">Acceuil </h1>
+        <h2 className="titlebloc__subtitle">Découvrez les derniers posts !</h2>
+      </div>
+      {isDataLoading ? (
+        <div className="loaderWrapper">
+          <Loader />
+        </div>
+      ) : (
+        <div className="cardsContainer">
+          {postsList.map((post, index) => (
+            <Card
+              key={`${post}-${index}`}
+              title={post.title}
+              picture={post.picture}
+              postContent={post.postContent}
+              creationDate={post.creationDate}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default HomePage;
