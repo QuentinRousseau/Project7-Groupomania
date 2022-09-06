5e00400ed8d7f924e041b401c6e9f6483296783f
Stylys <rousseau.quentin91580@gmail.com>
2022-09-06 17:17:02 +0200

    Modif Model of postSchema

--- src/api/models/Post.js
+++ src/api/models/Post.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+/*
 const postSchema = mongoose.Schema({
   userId: { type: String, required: true },
   title: { type: String, required: true },
@@ -11,6 +12,24 @@ const postSchema = mongoose.Schema({
   usersLiked: { type: [String] },
   usersDisliked: { type: [String] },
 });
+const Post = mongoose.models.Post || mongoose.model("Post", postSchema);*/
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  postContent: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  creationDate: { type: Number, required: true },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] },
+  postedBy: {
+    //recupere l'user concerné par la création du post et l'affiche
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
 const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
 
 export default Post;
