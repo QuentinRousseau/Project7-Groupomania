5a90372787d1f12b2e3370d57512aa1a6728f13c
Stylys <79668829+QuentinRousseau@users.noreply.github.com>
2024-10-31 13:59:19 +0100

    debug for production

--- src/api/controllers/images.js
+++ src/api/controllers/images.js
@@ -1,4 +1,5 @@
 export async function postImage(req, res, next) {
+  console.warn(`voici l'URL de requete : ${req.get("host")}/images`)
   let imageUrl = `${req.protocol}://${req.get("host")}/images/${req.file.filename
     }`; //create url file with the name
   res.status(201).json({ imageUrl: imageUrl });
