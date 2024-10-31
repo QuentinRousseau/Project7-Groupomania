c376f8ab15c9f2b8e892fe27109113ce1771d5f9
Stylys <79668829+QuentinRousseau@users.noreply.github.com>
2024-10-31 13:01:07 +0100

    check for deployment

--- src/api/routes/images.js
+++ src/api/routes/images.js
@@ -2,12 +2,13 @@ import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
-import auth from "../middleware/auth.js";
 import * as Ctrl from "../controllers/images.js";
+import auth from "../middleware/auth.js";
 import multer from "../middleware/multer-config.js";
 
 const router = express.Router();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(`voici la valeur de dirname ${__dirname}`)
 
 router.get("/", express.static(path.join(__dirname, "images")));
 router.post("/", multer, auth, Ctrl.postImage);
