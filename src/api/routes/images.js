83c7c3faa6d929eb095d3a191d2c11fecb993aa1
Stylys <79668829+QuentinRousseau@users.noreply.github.com>
2024-10-31 15:10:36 +0100

    logs to find images file

--- src/api/routes/images.js
+++ src/api/routes/images.js
@@ -6,7 +6,7 @@ import auth from "../middleware/auth.js";
 import multer from "../middleware/multer-config.js";
 
 const router = express.Router();
-console.warn(`voici le `)
+console.warn(`voici le path pour récupérer les images ${path.resolve("./images")}`)
 
 router.get("/", express.static(path.resolve("./images")));
 router.post("/", multer, auth, Ctrl.postImage);
